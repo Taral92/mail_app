90b39c8df9578345681bd649acb9a6bb6850cdcb
Taral92 <taru.patel37225@gmail.com>
2025-03-20 15:01:55 +0530

    x

--- Frontend/Gmail/src/components/Sidebar.jsx
+++ Frontend/Gmail/src/components/Sidebar.jsx
@@ -44,7 +44,7 @@ const Sidebar = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className="lg:w-[15%] sm:w-[15%] md:w-[15%] py-1">
+    <div className="lg:h-[100vh] sm:h-[100vh] md:h-[100vh]  py-1">
       <div className="px-3">
         <button
           onClick={() => dispatch(remove())}
